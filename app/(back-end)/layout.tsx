9954bb0e4b3668b4dbf0dead148ab839dea11a52
mosespace <kiskayemoses@gmail.com>
2024-12-15 10:26:56 +0000

    feat: commented out bread-crumb

--- app/(back-end)/layout.tsx
+++ app/(back-end)/layout.tsx
@@ -44,7 +44,7 @@ export default async function Layout({
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
+            {/* <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem className="hidden md:block">
                   <BreadcrumbLink href="#">
@@ -56,7 +56,7 @@ export default async function Layout({
                   <BreadcrumbPage>Data Fetching</BreadcrumbPage>
                 </BreadcrumbItem>
               </BreadcrumbList>
-            </Breadcrumb>
+            </Breadcrumb> */}
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-7 pt-0">{children}</div>
