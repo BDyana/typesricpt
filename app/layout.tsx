38a5bd041660550747ffaaf34ef46fc035ef4c3f
BDyana <168805526+BDyana@users.noreply.github.com>
2024-12-22 12:14:36 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,15 +29,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <!html lang="en">
+    <html lang="en">
       <head>
-        // Google Tag Manager //
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-T5GV6KQX');</script>
-        // End Google Tag Manager //
+        <meta
+          name="facebook-domain-verification"
+          content="pzi59zqi0qrjrv2rbtdiflwivnynhz"
+        />
+        <meta
+          name="google-site-verification"
+          content="MZrG05ej8tcAbK1d7W9RCA9JTHIG5Z8zdz_EHKMycfI"
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -55,11 +56,6 @@ export default function RootLayout({
           {children}
           <Analytics />
         </Providers>
-
-        // Google Tag Manager (noscript) //
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T5GV6KQX"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        // End Google Tag Manager (noscript) //
       </body>
     </html>
   );
