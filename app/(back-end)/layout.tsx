b84e419a39f310acc73b40c14226b973e6cd308d
BDyana <168805526+BDyana@users.noreply.github.com>
2024-12-25 22:10:39 +0600

    style change

--- app/(back-end)/layout.tsx
+++ app/(back-end)/layout.tsx
@@ -40,13 +40,13 @@ export default async function Layout({
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2">
-          <div className="flex items-center gap-2 px-4">
+        <header className="flex h-12 shrink-0 items-center gap-2">
+          <div className="flex items-center">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-1 pt-0">{children}</div>
+        <div className="flex flex-1 flex-col gap-4 pt-0">{children}</div>
       </SidebarInset>
     </SidebarProvider>
   );
