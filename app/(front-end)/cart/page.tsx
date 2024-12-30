d065fc03e44543b260db73ee1cdcee9842a1122b
mosespace <kiskayemoses@gmail.com>
2024-12-30 21:38:21 +0000

    feat: initial commit

--- app/(front-end)/cart/page.tsx
+++ app/(front-end)/cart/page.tsx
@@ -14,7 +14,7 @@ export default async function page() {
   const userProfile = await getUserProfile(userId);
 
   if (!session) {
-    redirect('/');
+    redirect('/login');
   }
   if (!session) {
     return;
