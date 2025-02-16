edb6111b4dfbf81bd10870aae0ac273b9b621869
BDyana <168805526+BDyana@users.noreply.github.com>
2025-02-16 16:05:45 +0600

    update sidebar menu

--- components/(front-end)/sidebar-menu.tsx
+++ components/(front-end)/sidebar-menu.tsx
@@ -44,38 +44,40 @@ export default async function SidebarMenu() {
               <ChevronRight className="mr-1 shrink-0 w-4 h-4 right-0 absolute hidden group-hover:block" />
             </Link>
             <ul className="absolute left-[275px] top-0 z-10 w-full px-3 py-1 bg-white h-[340px] hidden group-hover:block">
-              <h3 className="mb-1">Thread/Yarn</h3>
-              <li className="py-1">
-                <Link
-                  href="/categories/wool-thread"
-                  className="hover:text-black transition-all"
-                >
-                  Wool Thread
-                </Link>
-              </li>
-              <li className="py-0.5">
-                <Link className="hover:text-black transition-all" href="">
-                  Samarian Thread
-                </Link>
-              </li>
-              <li className="py-0.5">
-                <Link className="hover:text-black transition-all" href="">
-                  Malaikot Thread
-                </Link>
-              </li>
-              <li className="py-0.5">
-                <Link className="hover:text-black transition-all" href="">
-                  Crochet Thread
-                </Link>
-              </li>
-              <li className="py-0.5">
-                <Link
-                  className="hover:text-black transition-all"
-                  href="/categories/lacchi-suta"
-                >
-                  Lacchi Thread
-                </Link>
-              </li>
+              <div>
+                <h3 className="mb-1">Thread/Yarn</h3>
+                <li className="">
+                  <Link
+                    href="/categories/wool-thread"
+                    className="hover:text-black transition-all"
+                  >
+                    Wool Thread
+                  </Link>
+                </li>
+                <li className="">
+                  <Link className="hover:text-black transition-all" href="">
+                    Samarian Thread
+                  </Link>
+                </li>
+                <li className="">
+                  <Link className="hover:text-black transition-all" href="">
+                    Malaikot Thread
+                  </Link>
+                </li>
+                <li className="">
+                  <Link className="hover:text-black transition-all" href="">
+                    Crochet Thread
+                  </Link>
+                </li>
+                <li className="">
+                  <Link
+                    className="hover:text-black transition-all"
+                    href="/categories/lacchi-suta"
+                  >
+                    Lacchi Thread
+                  </Link>
+                </li>
+              </div>
             </ul>
           </li>
           <li className="">
@@ -87,14 +89,28 @@ export default async function SidebarMenu() {
               <span className="text-sm">Computer & Hardware</span>
             </Link>
           </li>
-          <li className="">
+          <li className="group">
             <Link
               href="/categories/gadget-accessories"
               className="pl-3 py-1 flex items-center gap-3 hover:bg-slate-100 duration-300 transition-all"
             >
               <Cpu size={16} />
-              <span className="text-sm">Gadget & Accessories</span>
+              <span className="text-sm">Mobile & Gadget</span>
+              <ChevronRight className="mr-1 shrink-0 w-4 h-4 right-0 absolute hidden group-hover:block" />
             </Link>
+            <ul className="absolute left-[275px] top-0 z-10 w-full px-3 py-1 bg-white h-[340px] hidden group-hover:block">
+              <div>
+                <h3 className="mb-1">SmartPhone Brand</h3>
+                <li className="">
+                  <Link
+                    href="/categories/smartphone"
+                    className="hover:text-black transition-all"
+                  >
+                    Infinix
+                  </Link>
+                </li>
+              </div>
+            </ul>
           </li>
           <li className="">
             <Link
@@ -112,29 +128,75 @@ export default async function SidebarMenu() {
             >
               <BookHeart size={16} />
               <span className="text-sm">Women's & Girls' Fashion</span>
+              <ChevronRight className="mr-1 shrink-0 w-4 h-4 right-0 absolute hidden group-hover:block" />
             </Link>
-            <ul className="absolute left-[275px] top-0 z-10 w-full px-3 py-1 bg-white h-[340px] hidden group-hover:block">
-              <h3 className="mb-1">Ladies Bag</h3>
-              <li className="hover:text-blue-700">
-                <Link className="hover:text-blue-700" href="">
-                  Party Bag
-                </Link>
-              </li>
-              <li className="hover:text-blue-700">
-                <Link className="hover:text-blue-700" href="">
-                  Canvas Bag
-                </Link>
-              </li>
-              <li className="hover:text-blue-700">
-                <Link className="hover:text-blue-700" href="">
-                  Hand Bag
-                </Link>
-              </li>
-              <li className="hover:text-blue-700">
-                <Link className="hover:text-blue-700" href="">
-                  Casual Bag
-                </Link>
-              </li>
+            <ul className="absolute left-[275px] top-0 z-10 w-full px-3 py-1 bg-white h-[340px] hidden group-hover:block space-y-3 text-gray-600">
+              <div>
+                <h3 className="mb-1 text-gray-800">Beauty & Bodycare</h3>
+                <li className="hover:text-blue-700">
+                  <Link className="hover:text-blue-700" href="/categories/cleanser">
+                    Cleanser
+                  </Link>
+                </li>
+                <li className="hover:text-blue-700">
+                  <Link className="hover:text-blue-700" href="/categories/facewash">
+                    Facewash
+                  </Link>
+                </li>
+                <li className="hover:text-blue-700">
+                  <Link className="hover:text-blue-700" href="/categories/hair-oil">
+                    Hair Oil
+                  </Link>
+                </li>
+                <li className="hover:text-blue-700">
+                  <Link className="hover:text-blue-700" href="/categories/toner">
+                    Toner
+                  </Link>
+                </li>
+                <li className="hover:text-blue-700">
+                  <Link className="hover:text-blue-700" href="/categories/serum">
+                    Serum
+                  </Link>
+                </li>
+                <li className="hover:text-blue-700">
+                  <Link className="hover:text-blue-700" href="/categories/moisturiser-cream">
+                    Moisturiser Cream
+                  </Link>
+                </li>
+                <li className="hover:text-blue-700">
+                  <Link className="hover:text-blue-700" href="/categories/moisturising-lotion">
+                    Moisturising Lotion
+                  </Link>
+                </li>
+                <li className="hover:text-blue-700">
+                  <Link className="hover:text-blue-700" href="/categories/repair-cream">
+                    Repair Cream
+                  </Link>
+                </li>
+              </div>
+              <div>
+                <h3 className="mb-1">Ladies Bag</h3>
+                <li className="hover:text-blue-700">
+                  <Link className="hover:text-blue-700" href="">
+                    Party Bag
+                  </Link>
+                </li>
+                <li className="hover:text-blue-700">
+                  <Link className="hover:text-blue-700" href="">
+                    Canvas Bag
+                  </Link>
+                </li>
+                <li className="hover:text-blue-700">
+                  <Link className="hover:text-blue-700" href="">
+                    Hand Bag
+                  </Link>
+                </li>
+                <li className="hover:text-blue-700">
+                  <Link className="hover:text-blue-700" href="">
+                    Casual Bag
+                  </Link>
+                </li>
+              </div>
             </ul>
           </li>
           <li className="group">
@@ -166,23 +228,132 @@ export default async function SidebarMenu() {
               </li>
             </ul>
           </li>
-          <li className="">
+          <li className="group">
             <Link
               href="/categories/grocery"
               className="pl-3 py-1 flex items-center gap-3 hover:bg-slate-100 duration-300 transition-all"
             >
               <ShoppingBasket size={16} />
               <span className="text-sm">Grocery & Beverage</span>
+              <ChevronRight className="mr-1 shrink-0 w-4 h-4 right-0 absolute hidden group-hover:block" />
             </Link>
+            <ul className="absolute left-[275px] top-0 z-10 w-full px-3 py-1 bg-white h-[340px] hidden group-hover:block space-y-3">
+              <div className="">
+                <h3 className="mb-1">Grocery</h3>
+                <li className="hover:text-blue-700">
+                  <Link
+                    className="hover:text-blue-700"
+                    href="/categories/rice"
+                  >
+                    Rice
+                  </Link>
+                </li>
+              </div>
+              <div className="">
+                <h3 className="mb-1">Beverage</h3>
+                <li className="hover:text-blue-700">
+                  <Link
+                    className="hover:text-blue-700"
+                    href="/categories/coffee"
+                  >
+                    Coffee
+                  </Link>
+                </li>
+                <li className="hover:text-blue-700">
+                  <Link
+                    className="hover:text-blue-700"
+                    href="/categories/cerelac"
+                  >
+                    Apple Cider Viengar
+                  </Link>
+                </li>
+                <li className="hover:text-blue-700">
+                  <Link
+                    className="hover:text-blue-700"
+                    href="/categories/rice"
+                  >
+                    Dry Fruits
+                  </Link>
+                </li>
+                <li className="hover:text-blue-700">
+                  <Link
+                    className="hover:text-blue-700"
+                    href="/categories/cerelac"
+                  >
+                    Organic Food
+                  </Link>
+                </li>
+                <li className="hover:text-blue-700">
+                  <Link
+                    className="hover:text-blue-700"
+                    href="/categories/cerelac"
+                  >
+                    Coconut Oil
+                  </Link>
+                </li>
+                <li className="hover:text-blue-700">
+                  <Link
+                    className="hover:text-blue-700"
+                    href="/categories/cerelac"
+                  >
+                    Coconut Milk
+                  </Link>
+                </li>
+                <li className="hover:text-blue-700">
+                  <Link
+                    className="hover:text-blue-700"
+                    href="/categories/food-supplements"
+                  >
+                    Food Supplements
+                  </Link>
+                </li>
+              </div>
+            </ul>
           </li>
-          <li className="">
+          <li className="group">
             <Link
               href="#"
               className="pl-3 py-1 flex items-center gap-3 hover:bg-slate-100 duration-300 transition-all"
             >
               <Microwave size={16} />
               <span className="text-sm">Home Appliancce</span>
+              <ChevronRight className="mr-1 shrink-0 w-4 h-4 right-0 absolute hidden group-hover:block" />
             </Link>
+            <ul className="absolute left-[275px] top-0 z-10 w-full px-3 py-1 bg-white h-[340px] hidden group-hover:block space-y-3">
+              <div className="">
+                {/* <h3 className="mb-1">Grocery</h3> */}
+                <li className="hover:text-blue-700">
+                  <Link
+                    className="hover:text-blue-700"
+                    href="/categories/water-heater"
+                  >
+                    Geyser
+                  </Link>
+                </li>
+                <li className="hover:text-blue-700">
+                  <Link
+                    className="hover:text-blue-700"
+                    href="/categories/rice-cooker"
+                  >
+                    Rice Cooker
+                  </Link>
+                </li>
+                <li className="hover:text-blue-700">
+                  <Link href="/categories/electric-blender"
+                  >
+                    Blender
+                  </Link>
+                </li>
+                <li className="hover:text-blue-700">
+                  <Link
+                    className="hover:text-blue-700"
+                    href="/categories/electric-kettle"
+                  >
+                    Kettle
+                  </Link>
+                </li>
+              </div>
+            </ul>
           </li>
           <li className="">
             <Link
