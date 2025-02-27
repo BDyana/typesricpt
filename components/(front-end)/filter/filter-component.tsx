a80826f0814ccbe0e80f23459a94dc645d0ebccb
BDyana <168805526+BDyana@users.noreply.github.com>
2025-02-27 18:50:11 +0600

    category page update

--- components/(front-end)/filter/filter-component.tsx
+++ components/(front-end)/filter/filter-component.tsx
@@ -27,7 +27,7 @@ export default function FilterComponent({
   slug,
 }: FilterComponentProps) {
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto">
       <Card className="mb-6">
         <CardContent className="py-4">
           <Sorting slug={slug} />
