7b6f191b690e643d02f89dd03debc1c1b0fb2cb1
BDyana <168805526+BDyana@users.noreply.github.com>
2025-03-05 18:59:35 +0600

    quill revert

--- components/re-usable-inputs/quill-editor.tsx
+++ components/re-usable-inputs/quill-editor.tsx
@@ -1,56 +1,47 @@
-"use client";
-import dynamic from "next/dynamic";
-import "react-quill-new/dist/quill.snow.css";
-import { Quill } from "react-quill-new"; // Explicitly import Quill
-
-const QuillNoSSRWrapper = dynamic(() => import("react-quill-new"), {
+'use client';
+import dynamic from 'next/dynamic';
+import 'react-quill-new/dist/quill.snow.css';
+const QuillNoSSRWrapper = dynamic(() => import('react-quill-new'), {
   ssr: false,
-  loading: () => <p>Loading...</p>,
+  loading: () => <p></p>,
 });
-
-// Ensure the "bullet" format is registered
-const List = Quill.import("formats/list");
-Quill.register(List, true);
-
 interface QuillEditorProps {
   label: string;
   className?: string;
   value: string;
   onChange: (value: string) => void;
 }
-
 export default function QuillEditor({
   label,
-  className = "sm:col-span-2",
+  className = 'sm:col-span-2',
   value,
   onChange,
 }: QuillEditorProps) {
   const modules = {
     toolbar: [
       [{ header: [1, 2, false] }],
-      ["bold", "italic", "underline", "strike", "blockquote"],
-      [{ list: "ordered" }, { list: "bullet" }], // Ensure bullet list is enabled
-      ["link", "color", "image"],
-      [{ "code-block": true }],
-      ["clean"],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      ['link', 'color', 'image'],
+      [{ 'code-block': true }],
+      ['clean'],
     ],
   };
-
   const formats = [
-    "header",
-    "bold",
-    "italic",
-    "underline",
-    "strike",
-    "blockquote",
-    "list",
-    "link",
-    "indent",
-    "image",
-    "code-block",
-    "color",
+    'header',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+    'link',
+    'indent',
+    'image',
+    'code-block',
+    'color',
   ];
-
   return (
     <div className={className}>
       <label
